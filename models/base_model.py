881acf9084fd3e4234121fdfbcaee746e4c00166
Ukewuihe Marinus <dubemarinus@gmail.com>
2024-03-19 15:13:48 -0700

    New commits

--- models/base_model.py
+++ models/base_model.py
@@ -1,58 +1,54 @@
 #!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
+"""This is the base model class for AirBnB"""
+from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
-import models
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime
-
 
 Base = declarative_base()
 
 
 class BaseModel:
-    """This class will defines all common attributes/methods
-    for other classes
-    """
-    __tablename__ = None
-
-    id = Column(String(60), nullable=False, primary_key=True)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    """This class will define all common attributes/methods for
+    other classes"""
+    id = Column(String(60), primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            self.id = kwargs.pop('id', str(uuid.uuid4()))
-            self.updated_at = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            self.created_at = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+        """Instantiation of base model class"""
+        if kwargs:
             for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 setattr(self, key, value)
+        if not self.id:
+            self.id = str(uuid.uuid4())
 
     def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = type(self).__name__
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        """Return string representation"""
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.to_dict())
+
+    def __repr__(self):
+        """Return string representation"""
+        return self.__str__()
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
-        from models import storage
-        self.updated_at = datetime.now()
-        storage.save()
+        """Update updated_at attribute"""
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = self.__dict__.copy()
-        dictionary['__class__'] = type(self).__name__
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        dictionary.pop(''_sa_instance_state), None
-        return dictionary
+        """Return dictionary representation"""
+        my_dict = dict(self.__dict__)
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        my_dict.pop('_sa_instance_state', None)
+        return my_dict
+
+    def delete(self):
+        """Delete the instance"""
+        models.storage.delete(self)
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,14 +2,16 @@
 
 from os import getenv
 from sqlalchemy import create_engine
-from sqlalchemy import sessionmaker. scoped_session
+from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import BaseModel, Base
+from models.state import State
+from models.city import City
 
 
 class DBStorage:
     """This class manages storage of hbnb models in a MySQL database."""
     __engine = None
-    __session = NOne
+    __session = None
 
     def __init__(self):
         """Initializes BDStorage instance."""
