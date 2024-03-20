0fc8fdf52590f6bb408c50c52023da7ba2105186
meekdee <drewaju.16@gmail.com>
2024-03-20 06:54:29 -0700

    D 13

--- models/base_model.py
+++ models/base_model.py
@@ -21,9 +21,18 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, key, value)
-        if not self.id:
+                if key != "__class__":
+                    setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now
+            if "updated_at" not in kwargs:
+                self.update_at = datetime.now
+        else:
             self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+
 
     def __str__(self):
         """Return string representation"""
