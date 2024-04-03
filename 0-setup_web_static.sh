3fe46d9af43ebe24fd80dbfffdc7f5678ffbe238
Ukewuihe Marinus <dubemarinus@gmail.com>
2024-04-03 07:49:52 -0700

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,17 @@
 #!/usr/bin/env bash
+# Sets up a web server for deployment of web_static.
 
-# Install Nginx if not installed
 apt-get update
-apt-get -y install nginx
+apt-get install -y nginx
 
-# Create necessary directories if they don't exist
-mkdir -p /data/web_static/releases/test /data/web_static/shared
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Create a fake HTML file
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-# Create symbolic link
-rm -rf /data/web_static/current
-ln -s /data/web_static/releases/test /data/web_static/current
+sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-# Give ownership of /data/ to ubuntu user and group
-chown -R ubuntu:ubuntu /data/
-
-# Update Nginx configuration
-config_file="/etc/nginx/sites-available/default"
-if grep -q 'location /hbnb_static' "$config_file"; then
-    sed -i 's#location /hbnb_static#location /hbnb_static {\n\talias /data/web_static/current/;\n}#' "$config_file"
-else
-    sed -i '/^\tlocation \/ {/a \\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' "$config_file"
-fi
-
-# Restart Nginx
 service nginx restart
