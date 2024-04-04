1cef74da5daf06cc6c9ff7160a94f7dd87a90238
Ukewuihe Marinus <dubemarinus@gmail.com>
2024-04-04 07:19:44 -0700

    New commits

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,37 +1,16 @@
-#!/usr/bin/env bash
+# Sets up a web server for deployment of web_static.
 
-# Install Nginx if not already installed
-if ! command -v nginx &> /dev/null; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
-fi
+apt-get update
+apt-get install -y nginx
 
-# Create necessary directories
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Create fake HTML file for testing
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-# Create symbolic link
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-# Set ownership
-sudo chown -R ubuntu:ubuntu /data
-
-# Update Nginx configuration
-sudo sed -i '/root \/var\/www\/html;/ a\
-\
-    location /hbnb_static {\
-        alias /data/web_static/current/;\
-        # If the requested file is not found, return a 404 error\
-        error_page 404 = /404.html;\
-    }' /etc/nginx/sites-available/default
-
-# Restart Nginx
-sudo service nginx restart
+service nginx restart
