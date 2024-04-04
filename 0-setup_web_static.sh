f08fcb94eb4cdd1672d10bb2647394cd7128c238
Ukewuihe Marinus <dubemarinus@gmail.com>
2024-04-04 08:26:59 -0700

    New commits

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,39 @@
-# Sets up a web server for deployment of web_static.
+#!/usr/bin/env bash
 
-apt-get update
-apt-get install -y nginx
+# Install Nginx if not already installed
+if ! command -v nginx &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get -y install nginx
+fi
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Create necessary directories if they don't exist
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+# Create fake HTML file for testing
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
-sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+# Create symbolic link if it doesn't exist
+if [ ! -e /data/web_static/current ]; then
+    sudo ln -sf /data/web_static/releases/test /data/web_static/current
+fi
 
-service nginx restart
+# Set ownership
+sudo chown -R ubuntu:ubuntu /data
+
+# Update Nginx configuration
+sudo sed -i '/root \/var\/www\/html;/ a\
+\
+    location /hbnb_static {\
+        alias /data/web_static/current/;\
+        # If the requested file is not found, return a 404 error\
+        error_page 404 = /404.html;\
+    }' /etc/nginx/sites-available/default
+
+# Restart Nginx
+sudo service nginx restart
