b015b868f679887f4a8a67b1358005f64175df58
root <drewaju.16@gmail.com>
2024-04-24 17:24:14 -0700

    Dee

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -9,3 +9,5 @@ GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Grant SELECT privilege on the performance_schema database to the user
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+FLUSH PRIVILEDGES;
--- web_flask/README.md
+++ web_flask/README.md
@@ -1,8 +1,1 @@
-0x04. AirBnB clone - Web framework
-Python
-Back-end
-Webserver
-Flask
- Weight: 2
-
-Author: Azeez Kazeem
+# Web Framework with Flask
